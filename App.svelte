880eb241f6e439df4a49bdd5ba26b1342865401b
KwonTaekhyun <57590123+KwonTaekhyun@users.noreply.github.com>
2022-08-03 19:32:52 +0900

    Initial commit

---
+++ App.svelte
@@ -0,0 +1,26 @@
+<script>
+  import One from "./One.svelte";
+  import Two from "./Two.svelte";
+  import Three from "./Three.svelte";
+
+  const options = [
+    { category: "one", component: One },
+    { category: "two", component: Two },
+    { category: "three", component: Three }
+  ];
+
+  let selected = options[0];
+</script>
+
+<main>
+	<div>
+    <select bind:value={selected}>
+      { #each options as option }
+        <option value={option}>{option.category}</option>
+      { /each }
+    </select>
+	</div>
+  <div>
+    <svelte:component this={selected.component} />
+  </div>
+</main>
---
+++ Three.svelte
@@ -0,0 +1,3 @@
+<div>
+	<h2>Three</h2>
+</div>
